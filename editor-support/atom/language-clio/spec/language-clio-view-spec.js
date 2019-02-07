2dd648df16439c6f1a8ffa9a186e852555c2fb52
Pouya <persian.writer@gmail.com>
2019-02-07 09:53:38 +0100

    Split repository

--- editor-support/atom/language-clio/spec/language-clio-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageClioView from '../lib/language-clio-view';
-
-describe('LanguageClioView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
