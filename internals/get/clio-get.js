4d5042c77e0f5cf2f8a36acbbb6c64999eb7ee86
micheleriva <master@micheleriva.it>
2019-03-13 22:49:52 +0100

    wip on github integration

--- internals/get/clio-get.js
+++ internals/get/clio-get.js
@@ -2,10 +2,12 @@ const decompress = require('decompress');
 const tmp = require('tmp');
 const fs = require('fs');
 const fetch = require("node-fetch");
-const gitHubRegex = /^github\.com(\/\w+|\d|_|-){2}/gm;
+
+const gitHubRegex = /^github\.com\/(\w|\d|_|-).+\/(\d|\w|-|_).+$/gi;
+const versionRegex = /@(\d\.?){1,3}$/gi;
 
 async function get(argv) {
-  return argv.match(gitHubRegex) 
+  return gitHubRegex.test(argv) 
          ? fetchGitHub(argv) 
          : fetchFile(argv)
 }
@@ -21,10 +23,71 @@ async function fetchFile(argv) {
   tmpobj.removeCallback();
 }
 
+/**
+ * @method fetchGitHub
+ * @param {string} argv
+ * @returns {void} 
+ */
+
 async function fetchGitHub(argv) {
-  fetchFile({
-    url: `https://${argv}/archive/master.zip`
-  })
+  /**
+   * Check if required package exposes a specific
+   * version or not.
+   * Specific version must be exposed the following way:
+   * 
+   * $ clio get github.com/foo/bar@1.2.3
+   * 
+   * Specific version has to be downloaded in the following format:
+   * 
+   *  https://github.com/foo/bar/archive/1.2.3.zip
+   * 
+   * If no version is specified, download from master branch:
+   * 
+   * https://github.com/foo/bar/archive/master.zip
+   *
+   */
+  const packageTarget = hasVersion(argv) 
+                      ? getVersion(argv).replace("@", "")
+                      : "master";
+
+  const packageUri = hasVersion(argv)
+                   ? argv.replace(versionRegex, "")
+                   : argv
+
+  /**
+   * So now let's create a download uri that will look as follows:
+   * 
+   * https://github.com/archive/foo/bar/{master|@1.2.3}.zip
+   */
+  const fetchUrl = `https://${packageUri}/archive/${packageTarget}.zip`;
+  
+
+  process.stdout.write(`Downloading ${fetchUrl}...`);
+  
+  fetchFile({url: fetchUrl});
 }
 
-get("github.com/micheleriva/vscode-clio")
+/**
+ * @method getVersion
+ * @param {string} argv
+ * @returns {string}
+ */
+
+function getVersion(argv) {
+  const matches = argv.match(versionRegex)
+  return matches 
+       ? matches[0] 
+       : ""
+}
+
+/**
+ * @method hasVersion
+ * @param {string} argv
+ * @returns {boolean} 
+ */
+
+function hasVersion(argv) {
+  return !!getVersion(argv).length
+}
+
+get("github.com/micheleriva/mjn")
