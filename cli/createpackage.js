75cc338b573f37fad4cdf3a6d9fcfa53c0510d3a
garritfra <garritfranke@gmail.com>
2019-12-05 22:33:52 +0100

    Initialize git repository for new projects

--- cli/createpackage.js
+++ cli/createpackage.js
@@ -6,44 +6,26 @@ function createPackage(packageName) {
   const cwd = process.cwd();
   const dir = path.join(cwd, packageName);
   fs.mkdir(dir, () => {
-    exec(`pushd ${packageName}` + ` && npm init -y` + ` && popd`, () => {
-      const pkg = require(path.join(dir, "package.json"));
-      if (!pkg.clioDependencies) {
-        pkg.clioDependencies = ["stdlib"];
-      } else if (!pkg.clioDependencies.includes("stdlib")) {
-        pkg.clioDependencies.push("stdlib");
-      }
-      const stringified = JSON.stringify(pkg, null, 2);
-      return fs.writeFile(
-        path.join(dir, "package.json"),
-        stringified,
-        "utf8",
-        err => {
-          if (err) {
-            console.error(err);
-          }
-          console.log("Added Clio dependency");
-        }
-      );
-    });
+    childProcess.execSync(
+      `pushd ${packageName}` + ` && npm init -y` + ` && popd`
+    );
+    console.log("Initialized npm module");
+
+    const pkg = require(path.join(dir, "package.json"));
+    if (!pkg.clioDependencies) {
+      pkg.clioDependencies = ["stdlib"];
+    } else if (!pkg.clioDependencies.includes("stdlib")) {
+      pkg.clioDependencies.push("stdlib");
+    }
+    const stringified = JSON.stringify(pkg, null, 2);
+    fs.writeFileSync(path.join(dir, "package.json"), stringified, "utf8");
+    console.log("Added Clio dependencies");
+
+    childProcess.execSync(
+      `pushd ${packageName}` +
+        ` && git init && git add -A && git commit -m "Initial Commit" && popd`
+    );
   });
 }
-
-const exec = (command, callback) => {
-  const process = childProcess.exec(command);
-
-  process.stdout.on("data", data => {
-    console.log(data.toString());
-  });
-
-  process.stderr.on("data", data => {
-    console.error(data.toString());
-  });
-
-  process.on("exit", code => {
-    console.log("npm module initialized");
-    callback();
-  });
-};
 
 module.exports = createPackage;
