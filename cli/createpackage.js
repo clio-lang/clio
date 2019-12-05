51dde7b167f0029d548c4408c6e54da093a1bfe5
garritfra <garritfranke@gmail.com>
2019-12-05 22:14:07 +0100

    Add Clio deps to package.json of created project

--- cli/createpackage.js
+++ cli/createpackage.js
@@ -1,13 +1,35 @@
 const fs = require("fs");
 const childProcess = require("child_process");
 const path = require("path");
+
 function createPackage(packageName) {
-  fs.mkdir(path.join(process.cwd(), packageName), () => {
-    exec(`pushd ${packageName}` + ` && npm init -y` + ` && popd`);
+  const cwd = process.cwd();
+  const dir = path.join(cwd, packageName);
+  fs.mkdir(dir, () => {
+    exec(`pushd ${packageName}` + ` && npm init -y` + ` && popd`, () => {
+      const pkg = require(path.join(dir, "package.json"));
+      if (!pkg.clioDependencies) {
+        pkg.clioDependencies = ["stdlib"];
+      } else if (!pkg.clioDependencies.includes("stdlib")) {
+        pkg.clioDependencies.push("stdlib");
+      }
+      const stringified = JSON.stringify(pkg, null, 2);
+      return fs.writeFile(
+        path.join(dir, "package.json"),
+        stringified,
+        "utf8",
+        err => {
+          if (err) {
+            console.error(err);
+          }
+          console.log("Added Clio dependency");
+        }
+      );
+    });
   });
 }
 
-const exec = command => {
+const exec = (command, callback) => {
   const process = childProcess.exec(command);
 
   process.stdout.on("data", data => {
@@ -19,7 +41,8 @@ const exec = command => {
   });
 
   process.on("exit", code => {
-    console.log("Exited with code " + code.toString());
+    console.log("npm module initialized");
+    callback();
   });
 };
 
