d601661296bb122f03224f7faffd5b4f6a294e6c
garritfra <garritfranke@gmail.com>
2019-12-19 19:59:05 +0100

    Add versionUtils tests

---
+++ tests/utils/versionUtils.test.js
@@ -0,0 +1,22 @@
+const versionUtils = require("../../utils/versionUtils");
+
+describe("isSupportedNodeVersion", () => {
+  test("10.4 is supported", () => {
+    expect(versionUtils.isSupportedNodeVersion("10.4")).toBe(true);
+  });
+
+  test("10.6 is supported", () => {
+    expect(versionUtils.isSupportedNodeVersion("10.6")).toBe(true);
+  });
+
+  test("10.3 is not supported", () => {
+    expect(versionUtils.isSupportedNodeVersion("10.3")).toBe(false);
+  });
+  test("9.6 is not supported", () => {
+    expect(versionUtils.isSupportedNodeVersion("9.6")).toBe(false);
+  });
+
+  test("9.2 is not supported", () => {
+    expect(versionUtils.isSupportedNodeVersion("9.2")).toBe(false);
+  });
+});
