ed48d232f1178d9ba92dd483e530da426b2642ce
Pouya Eghbali <pouya.eghbali@yandex.com>
2019-12-19 22:11:05 +0100

    Use new parser in cli ast command (#53)
    * Use new parser in cli ast command
    
    * Fix missing exports
    
    * Return cst from printAst

--- cli/commands/ast.js
+++ cli/commands/ast.js
@@ -1,48 +1,28 @@
 const fs = require("fs");
 const treeify = require("treeify");
-const lexer = require("../../lexer/lexer");
-const parser = require("../../parser/parser");
+const { parser } = require("../../core/parser");
 
-exports.command = "ast <source>";
-exports.desc = "Print ast for a Clio file";
-exports.builder = {
+const readFile = async (...args) => fs.readFileSync(...args);
+const log = (...args) => console.log(...args) || args;
+
+const printAst = source =>
+  readFile(source, "utf8")
+    .then(parser)
+    .then(cst => treeify.asTree(cst, true))
+    .then(log)
+    .catch(console.trace);
+
+const command = "ast <source>";
+const desc = "Print ast for a Clio file";
+const builder = {
   source: { describe: "source file to analyze", type: "string" }
 };
-exports.handler = function(argv) {
-  console.log(printAst(argv.source));
+const handler = argv => printAst(argv.source);
+
+module.exports = {
+  command,
+  desc,
+  builder,
+  handler,
+  printAst
 };
-
-function removeProps(obj, keys) {
-  if (obj instanceof Array) {
-    obj.forEach(function(item) {
-      removeProps(item, keys);
-    });
-  } else if (typeof obj === "object") {
-    Object.getOwnPropertyNames(obj).forEach(function(key) {
-      if (keys.indexOf(key) !== -1) {
-        delete obj[key];
-      } else {
-        removeProps(obj[key], keys);
-      }
-    });
-  }
-}
-
-function printAst(source) {
-  try {
-    const contents = fs.readFileSync(source, "utf8");
-    let tokens = lexer(contents);
-    if (tokens[0] == false) {
-      return;
-    }
-    tokens = tokens[1];
-    const result = parser(contents, tokens, false, source);
-    let ast = result[1];
-    removeProps(ast, "index");
-    return treeify.asTree(ast, true);
-  } catch (err) {
-    console.trace(err);
-  }
-}
-
-exports.printAst = printAst;
--- tests/cli/misc/ast.test.js
+++ tests/cli/misc/ast.test.js
@@ -1,13 +1,12 @@
 const tmp = require("tmp");
-const path = require('path');
-const { createPackage } = require('../../../cli/commands/new');
-const { printAst } = require('../../../cli/commands/ast');
+const path = require("path");
+const { createPackage } = require("../../../cli/commands/new");
+const { printAst } = require("../../../cli/commands/ast");
 
 test("Print AST", async () => {
   const dir = tmp.dirSync();
   await createPackage(dir.name);
-  const source = path.join(dir.name, 'index.clio');
-  const ast = printAst(source);
-  expect(ast.toString()).toContain("tokens\n");
-  expect(ast.toString()).toContain("eof\n");
+  const source = path.join(dir.name, "index.clio");
+  const ast = await printAst(source);
+  expect(ast.pop()).toContain("name: clio\n");
 });
