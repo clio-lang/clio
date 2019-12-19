636420818f399f8bfc6d4bb7eecf1a7efdbc6595
garritfra <garritfranke@gmail.com>
2019-12-19 17:35:26 +0100

    Add warning about outdated node version

--- cli/index.js
+++ cli/index.js
@@ -3,6 +3,15 @@
 global.fetch = require("node-fetch"); // fetch is not implemented in node (yet)
 global.WebSocket = require("websocket").w3cwebsocket; // same for WebSocket
 
+const nodeFullVersion = process.versions.node.split(".");
+const nodeMajorVersion = Number(nodeFullVersion[0]);
+const nodeMinorVersion = Number(nodeFullVersion[1]);
+
+if (nodeMajorVersion < 10 && nodeMinorVersion < 4) {
+  console.warn(
+    "You're using a version of node that Clio does not support. This might cause problems. Please update node to the latest version."
+  );
+}
 require("yargs")
   .commandDir("commands")
   .help()
