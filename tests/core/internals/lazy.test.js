514b6f84852f98f80ad6df3b10ec687751887910
garritfra <garritfranke@gmail.com>
2019-12-23 13:51:38 +0100

    Add lazy tests

---
+++ tests/core/internals/lazy.test.js
@@ -0,0 +1,15 @@
+const { Lazy, lazy } = require("../../../core/internals/lazy");
+
+test("Lazy evaluates when valueOf() is called", () => {
+  const lzy = new Lazy(() => "foo");
+
+  expect(lzy.fn).toBeDefined();
+  expect(lzy.valueOf()).toBe("foo");
+});
+
+test("Lazy function wrapper evaluates when valueOf is called", () => {
+  const lzy = lazy(() => 2);
+
+  expect(lzy.fn).toBeDefined();
+  expect(lzy.valueOf()).toBe(2);
+});
