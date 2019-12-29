7ac567b457a3929dccfdd0fb53454a6253c9ac4c
Andrey Luiz <andreyluiz.developer@gmail.com>
2019-12-29 19:32:30 +0000

    Add some coverage

--- packages/internals/tests/array.test.js
+++ packages/internals/tests/array.test.js
@@ -1,4 +1,5 @@
 const { Array } = require("../src/array");
+const { io } = require("../src/io");
 
 test("Created array contains values", () => {
   const arr = new Array(1, 2, 3);
@@ -14,4 +15,19 @@ test("Array is mappable", () => {
   expect(mapped.items[0]).toBe(2);
   expect(mapped.items[1]).toBe(4);
   expect(mapped.items[2]).toBe(6);
+});
+
+test("Array valueOf", () => {
+  const arr = new Array(1, 2, 3);
+  const values = arr.valueOf();
+  expect(values).toEqual([1, 2, 3]);
+});
+
+test("Array is mappable with IO items", () => {
+  const first = io(jest.fn());
+  const second = io(jest.fn());
+  const arr = new Array(first, second);
+  const mappedValues = arr.map(i => i);
+  expect(mappedValues.items[0]).toEqual(first);
+  expect(mappedValues.items[1]).toEqual(second);
 });
