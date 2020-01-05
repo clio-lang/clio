1bc65a14481c1855b2f962c1926bde6c1777331a
Andrey Luiz <andreyluiz.developer@gmail.com>
2020-01-05 19:24:14 +0000

    Add platform specific implementations

--- package/npm_dependencies.js
+++ package/npm_dependencies.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 const { spawn } = require("child_process");
+const { getPackageConfig, CONFIGFILE_NAME } = require('./packageConfig');
 
 function fetchNpmDependencies(destination) {
   process.chdir(destination);
@@ -15,7 +16,19 @@ function hasInstalledNpmDependencies(destination) {
   return fs.existsSync(path.join(destination, "package-lock.json"));
 }
 
+function getParsedNpmDependencies(source) {
+  const dependencies = {};
+  const npmDependencies = getPackageConfig(path.join(source, CONFIGFILE_NAME)).npm_dependencies;
+  if (npmDependencies) {
+    npmDependencies.forEach(dep => {
+      dependencies[dep.name] = dep.version;
+    });
+  }
+  return dependencies;
+}
+
 module.exports = {
   fetchNpmDependencies,
-  hasInstalledNpmDependencies
+  hasInstalledNpmDependencies,
+  getParsedNpmDependencies
 };
