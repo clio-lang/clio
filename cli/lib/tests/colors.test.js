7a525026cee7de99e52e0c73c8a4a76a5bb6563c
garritfra <garritfranke@gmail.com>
2020-01-17 18:50:09 +0100

    Add progress tests

--- cli/lib/tests/colors.test.js
+++ cli/lib/tests/colors.test.js
@@ -1,5 +1,5 @@
 const chalk = require("chalk");
-const { trace, error, warn, success, info, brightRed } = require("../../../lib/colors");
+const { trace, error, warn, success, info, brightRed } = require("../colors");
 
 describe("Colored messages", () => {
   const defaultConsoleError = console.error;
@@ -25,38 +25,50 @@ describe("Colored messages", () => {
     const error = new Error("Testing trace");
     trace(error);
 
-    expect(console.trace).toHaveBeenCalledWith(brightRed("Error: Testing trace"));
+    expect(console.trace).toHaveBeenCalledWith(
+      brightRed("Error: Testing trace")
+    );
   });
 
   test("error", () => {
     const err = new Error("Testing error");
     error(err);
 
-    expect(console.error).toHaveBeenCalledWith(brightRed("Error: Testing error"));
+    expect(console.error).toHaveBeenCalledWith(
+      brightRed("Error: Testing error")
+    );
   });
 
   test("error with prefix", () => {
     const err = new Error("Testing error");
     error(err, "Foo");
 
-    expect(console.error).toHaveBeenCalledWith(brightRed("Foo Error: Testing error"));
+    expect(console.error).toHaveBeenCalledWith(
+      brightRed("Foo Error: Testing error")
+    );
   });
 
   test("warn", () => {
     warn("Hey, be careful.");
 
-    expect(console.warn).toHaveBeenCalledWith(chalk.yellow("Warning: Hey, be careful."));
+    expect(console.warn).toHaveBeenCalledWith(
+      chalk.yellow("Warning: Hey, be careful.")
+    );
   });
 
   test("success", () => {
     success("You did it!");
 
-    expect(console.log).toHaveBeenCalledWith(chalk.green("Success: You did it!"));
+    expect(console.log).toHaveBeenCalledWith(
+      chalk.green("Success: You did it!")
+    );
   });
 
   test("info", () => {
     info("Just looking around.");
 
-    expect(console.log).toHaveBeenCalledWith(chalk.blue("Info: Just looking around."));
+    expect(console.log).toHaveBeenCalledWith(
+      chalk.blue("Info: Just looking around.")
+    );
   });
 });
---
+++ cli/lib/tests/progress.test.js
@@ -0,0 +1,27 @@
+const { Progress } = require("../progress");
+
+describe("Progress", () => {
+  test("progress", () => {
+    const progress = new Progress();
+    expect(progress.progress).toBeDefined();
+  });
+
+  test("progress silent", () => {
+    const progress = new Progress(true);
+    expect(progress.progress).toBeNull();
+  });
+
+  test("fail", () => {
+    const progress = new Progress(false, "Foo");
+
+    progress.fail("Failed");
+    expect(progress.progress.isSpinning).toBe(false);
+  });
+
+  test("fail silent", () => {
+    const progress = new Progress(true, "Foo");
+
+    progress.fail("Failed");
+    expect(progress.progress).toBeNull();
+  });
+});
