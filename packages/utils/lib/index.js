a61136fffd114dabc9ca9f126c955dd43b83f6b1
garritfra <garritfranke@gmail.com>
2020-01-18 20:13:33 +0100

    Add documentation for native function

--- packages/utils/lib/index.js
+++ packages/utils/lib/index.js
@@ -1,3 +1,12 @@
 var addon = require("../native");
 
-exports.isSupportedNodeVersion = addon.isSupportedNodeVersion;
+/**
+ * Checks, if a given node version is officially supported by Clio.
+ * Note that that version is subject to change. In this case, tests may have to be adjusted
+ *
+ * @argument {string} versionStr in format "major.minor.patch". E.g.: "12.6.1"
+ * @returns {boolean} true, if node version is officially supported by Clio
+ */
+// Note: Wrapper is needed for documentation
+exports.isSupportedNodeVersion = versionStr =>
+  addon.isSupportedNodeVersion(versionStr);
