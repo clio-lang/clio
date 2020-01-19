e0b748d88b1eeb206c379d4670d6a1957ac83680
Pouya Eghbali <persian.writer@gmail.com>
2020-01-19 19:37:56 +0100

    Fix array tests

--- packages/internals/tests/range.test.js
+++ packages/internals/tests/range.test.js
@@ -2,7 +2,7 @@ const { Range } = require("../src/range");
 
 test("Created range equals array", () => {
   const range = new Range({ start: 0, end: 10, step: 1 });
-  const arr = range.valueOf();
+  const arr = range.toArray();
   const expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   expect(arr.items.every((item, index) => item == expected[index])).toBe(true);
   expect(arr.items.length).toBe(expected.length);
@@ -11,7 +11,7 @@ test("Created range equals array", () => {
 
 test("Range with steps", () => {
   const range = new Range({ start: 0, end: 10, step: 2 });
-  const arr = range.valueOf();
+  const arr = range.toArray();
   const expected = [0, 2, 4, 6, 8];
   expect(arr.items.every((item, index) => item == expected[index])).toBe(true);
   expect(arr.items.length).toBe(expected.length);
@@ -20,7 +20,7 @@ test("Range with steps", () => {
 
 test("Range with negative steps", () => {
   const range = new Range({ start: 0, end: -10, step: -2 });
-  const arr = range.valueOf();
+  const arr = range.toArray();
   const expected = [0, -2, -4, -6, -8];
   expect(arr.items.every((item, index) => item == expected[index])).toBe(true);
   expect(arr.items.length).toBe(expected.length);
@@ -28,10 +28,9 @@ test("Range with negative steps", () => {
 });
 
 test("Map function to range", () => {
-  const range = new Range({ start: 0, end: 10, step: 2 }).map(n => n * 2);
-  const arr = range.valueOf();
+  const arr = new Range({ start: 0, end: 10, step: 2 }).map(n => n * 2);
   const expected = [0, 4, 8, 12, 16];
   expect(arr.items.every((item, index) => item == expected[index])).toBe(true);
   expect(arr.items.length).toBe(expected.length);
-  expect(range.length).toBe(expected.length);
+  expect(arr.length).toBe(expected.length);
 });
