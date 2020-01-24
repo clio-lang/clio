b62bcd072531c10fa1e900725315bb302ce0c106
garritfra <garritfranke@gmail.com>
2020-01-24 21:35:14 +0100

    Add run with dependency test

--- cli/commands/tests/run.test.js
+++ cli/commands/tests/run.test.js
@@ -1,11 +1,30 @@
 const tmp = require("tmp");
-const { createPackage } = require('../../../cli/commands/new');
-const { run } = require('../../../cli/commands/run');
+const path = require("path");
+const { createPackage } = require("../../../cli/commands/new");
+const { run } = require("../../../cli/commands/run");
+const deps = require("../../../cli/commands/deps_commands/get");
+const packageConfig = require("../../../package/packageConfig");
 
 test("Runs hello world", async () => {
   console.log = jest.fn();
   const dir = tmp.dirSync();
   await createPackage(dir.name);
   await run(dir.name);
-  expect(console.log).toHaveBeenCalledWith('Hello World');
+  dir.removeCallback();
+  expect(console.log).toHaveBeenCalledWith("Hello World");
+});
+
+test("Runs a project with dependencies", async () => {
+  console.log = jest.fn();
+  const dir = tmp.dirSync();
+  await createPackage(dir.name);
+  const config = packageConfig.getPackageConfig(
+    path.join(dir.name, "clio.toml")
+  );
+  config.dependencies.push({ name: "hub:add", version: "latest" });
+  packageConfig.writePackageConfig(config, dir.name);
+  await deps.handler();
+  await run(dir.name);
+  await dir.removeCallback();
+  expect(console.log).toHaveBeenCalled();
 });
