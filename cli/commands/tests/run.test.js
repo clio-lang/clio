fc105a733d277f7138881aaf359bc1d074340c8c
garritfra <garritfranke@gmail.com>
2020-01-24 19:57:19 +0100

    Fix run test

--- cli/commands/tests/run.test.js
+++ cli/commands/tests/run.test.js
@@ -1,24 +1,11 @@
-/*
------------------------------
-Disabling this for now
-I haven't updated the run
-command yet
------------------------------
 const tmp = require("tmp");
-const path = require('path');
 const { createPackage } = require('../../../cli/commands/new');
 const { run } = require('../../../cli/commands/run');
 
 test("Runs hello world", async () => {
   console.log = jest.fn();
   const dir = tmp.dirSync();
-  await createPackage(dir.name, true);
-  const source = path.join(dir.name, 'index.clio');
-  await run(source);
+  await createPackage(dir.name);
+  await run(dir.name);
   expect(console.log).toBeCalledWith('Hello World');
-});
-*/
-
-test("Runs hello world", async () => {
-  expect(true).toEqual(true);
 });
