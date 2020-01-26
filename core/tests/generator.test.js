64daf56438a0db34843f936f299dde28bbcab2e5
Pouya Eghbali <persian.writer@gmail.com>
2020-01-26 16:19:32 +0100

    Added generator test

--- core/tests/generator.test.js
+++ core/tests/generator.test.js
@@ -27,4 +27,29 @@ import this from "that"
 import this as that from "other"
 import "module"
 import "module" as m
+
+"Get one element" -> print
+[1 2 3 4][0] -> print
+[1:4][0] -> print
+
+"\nSlice with range" -> print
+[1 2 3 4][0:2] -> print
+[1:4][0:2] -> print
+
+"\nSlice with stepped range" -> print
+[1 2 3 4 5 6 7 8 9 10][10:0:-2] -> print
+[0:11][10:0:-2] -> print
+
+"\nSlice with array" -> print
+[1 2 3 4 5 6 7 8][[1 2 3]] -> print
+[0:11][[1 2 3]] -> print
+
+"\nMultidimensional slicing" -> print
+[[1 2 3]
+ [4 5 6]
+ [7 8 9]][[0:2] 1] -> print
+
+[[1 2 3]
+ [4 5 6]
+ [7 8 9]][[0:2] [1]] -> print
 `;
