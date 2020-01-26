5fb44bad29df430720c3465fd8d2cf7f397909c2
Pouya Eghbali <persian.writer@gmail.com>
2020-01-26 16:35:08 +0100

    Added tests for Range.slice

--- packages/internals/tests/range.test.js
+++ packages/internals/tests/range.test.js
@@ -1,4 +1,5 @@
 const { Range } = require("../src/range");
+const { Array } = require("../src/array");
 
 test("Created range equals array", () => {
   const range = new Range({ start: 0, end: 10, step: 1 });
@@ -33,4 +34,22 @@ test("Map function to range", () => {
   expect(arr.items.every((item, index) => item == expected[index])).toBe(true);
   expect(arr.items.length).toBe(expected.length);
   expect(arr.length).toBe(expected.length);
+});
+
+test("Slice range (with a number)", () => {
+  const range = new Range({ end: 10 });
+  const slice = range.slice(new Array(0));
+  expect(slice).toEqual(0);
+});
+
+test("Slice range (with a range)", () => {
+  const range = new Range({ end: 10 });
+  const slice = range.slice(new Range({ start: 1, end: 3 }));
+  expect(slice.items).toEqual([1, 2]);
+});
+
+test("Slice range (with an array)", () => {
+  const range = new Range({ end: 10 });
+  const slice = range.slice(new Array(new Array(1, 2)));
+  expect(slice.items).toEqual([1, 2]);
 });
