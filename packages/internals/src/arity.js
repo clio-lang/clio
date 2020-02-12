cac5c2634264449207e521a9b77d4a6730990e94
Pouya Eghbali <persian.writer@gmail.com>
2020-02-12 20:33:50 +0100

    Add documentation to arity functions

--- packages/internals/src/arity.js
+++ packages/internals/src/arity.js
@@ -1,6 +1,16 @@
 const toString = Object.prototype.toString;
 const fnToString = Function.prototype.toString;
+/**
+ * Regex to match host constructors (Safari > 4)
+ * Taken from https://davidwalsh.name/detect-native-function
+ * @type {RegExp}
+ */
 const reHostCtor = /^\[object .+?Constructor\]$/;
+/**
+ * Regex to match native function declarations
+ * Taken from https://davidwalsh.name/detect-native-function
+ * @type {RegExp}
+ */
 const reNative = RegExp(
   "^" +
     String(toString)
@@ -9,6 +19,11 @@ const reNative = RegExp(
     "$"
 );
 
+/**
+ * Check if a function is a native function
+ * @param {Function} fn Function to check
+ * @returns {Boolean}
+ */
 const isNative = fn => {
   const type = typeof fn;
   return type == "function"
@@ -16,7 +31,13 @@ const isNative = fn => {
     : (fn && type == "object" && reHostCtor.test(toString.call(fn))) || false;
 };
 
-const getParenEnd = source => {
+/**
+ * Parses a function source code, returning index of
+ * the place where function parameter definition ends
+ * @param {String} source source code of a function
+ * @returns {Number} index where parameter definition ends
+ */
+const getParamEnd = source => {
   let i = 0,
     lpar = 0,
     rpar = 0;
@@ -29,10 +50,15 @@ const getParenEnd = source => {
   }
 };
 
+/**
+ * Checks how many arguments a function accepts
+ * @param {Function} fn function to check
+ * @returns {Number|Infinity} arity of the function
+ */
 const getArity = fn => {
   const source = fn.toString();
   if (isNative(fn)) return fn.length;
-  const end = getParenEnd(source);
+  const end = getParamEnd(source);
   let paramText = source.slice(0, end);
   paramText = paramText.replace(/^[^(]*?\(/, "").replace(/\) *$/, "");
   paramText = paramText.replace(/(['"])([^\\]|\\.)*?\1/g, "");
