1f6c87cc8df3a4116786aaf62f0bf2d07f46efed
Pouya Eghbali <persian.writer@gmail.com>
2020-02-17 21:41:47 +0100

    Document the Rule class

--- packages/core/generator/rule.js
+++ packages/core/generator/rule.js
@@ -1,6 +1,16 @@
 const { SourceNode } = require("source-map");
 
+/**
+ * Class representing a rule.
+ */
 class Rule {
+  /**
+   * Create a rule
+   * @param {Object} cst CST node returned by parser
+   * @param {String} file File name of the source
+   * @param {Function} generate generate function (from generator)
+   * @returns {Void}
+   */
   constructor(cst, file, generate) {
     this.cst = cst;
     this.file = file;
@@ -10,9 +20,16 @@ class Rule {
     this.line = line;
     this.column = column;
   }
+  /**
+   * Override this when extending Rule class
+   */
   parseCST() {
     // override this
   }
+  /**
+   * Wraps the return value of parseCST method in a SourceNode object
+   * @returns {SourceNode}
+   */
   getNode() {
     return new SourceNode(this.line, this.column, this.file, this.parseCST());
   }
