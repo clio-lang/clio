0135db9d10b3633eebc3f3c479e1c13d228aa312
Pouya Eghbali <persian.writer@gmail.com>
2020-02-23 21:50:30 +0100

    Fix mathematical ops

--- packages/core/generator/rules/math.js
+++ packages/core/generator/rules/math.js
@@ -1,16 +1,22 @@
 const { Rule } = require("../rule");
 const arr = require("../arr");
 
-const make = (left, op, right) => arr`(${left} ${op} ${right})`;
-const makePow = (left, right) => arr`Math.pow(${left}, ${right})`;
+const make = (op, left, right) => arr`builtins.${op}(${left}, ${right})`;
+
+const opMap = {
+  "+": "add",
+  "-": "sub",
+  "*": "mul",
+  "/": "div",
+  "^": "pow"
+};
 
 class math extends Rule {
   parseCST() {
     const { lhs, op, rhs } = this.cst;
     const left = this.generate(lhs);
     const right = this.generate(rhs);
-    if (op != "^") return make(left, op, right);
-    return makePow(left, right);
+    return make(opMap[op], left, right);
   }
 }
 
