5232fb727d3d781ce374e6102d71ccc196b70472
Pouya Eghbali <pouya.eghbali@yandex.com>
2020-02-24 13:08:11 +0100

    Add Clio tests

--- packages/core/parser.js
+++ packages/core/parser.js
@@ -4,40 +4,41 @@ const model = require("./model.js");
 const { colorize } = require("clio-highlight");
 
 const parse = tokens => bean(model, tokens);
+const handleParseResult = ([success, result]) => {
+  if (success) {
+    const cst = result[0];
+    return cst;
+  } else {
+    const firstUnmatched = result[0].name;
+    const expecting = model
+      .filter(m => m.left == firstUnmatched)
+      .map(({ right }) => right);
+    const encountered = result[1].name;
+    const { location } = result[1];
+    const { line, column } = location;
+    const lineIndex = line - 1;
+    const lines = source
+      .split("\n")
+      .slice(Math.max(0, lineIndex - 3), lineIndex + 1);
+    const lineCount = lines.length;
+    console.log();
+    lines.forEach((rawLine, index) => {
+      const lineNumber = line + 1 - lineCount + index;
+      const padding = " ".repeat((line - lineNumber).toString().length + 1);
+      const coloredLine = colorize(rawLine);
+      console.log(`${padding}${lineNumber} | ${coloredLine}`);
+    });
+    console.log(" ".repeat(column + 4 + line.toString().length) + "^");
+    const ParsingError = `At [${line},${column}]: Expecting one of ${expecting.join(
+      ", "
+    )} but encountered ${encountered}`;
+    throw ParsingError;
+  }
+};
 
 const parser = source =>
   lexer(source)
     .then(parse)
-    .then(([success, result]) => {
-      if (success) {
-        const cst = result[0];
-        return cst;
-      } else {
-        const firstUnmatched = result[0].name;
-        const expecting = model
-          .filter(m => m.left == firstUnmatched)
-          .map(({ right }) => right);
-        const encountered = result[1].name;
-        const { location } = result[1];
-        const { line, column } = location;
-        const lineIndex = line - 1;
-        const lines = source
-          .split("\n")
-          .slice(Math.max(0, lineIndex - 3), lineIndex + 1);
-        const lineCount = lines.length;
-        console.log();
-        lines.forEach((rawLine, index) => {
-          const lineNumber = line + 1 - lineCount + index;
-          const padding = " ".repeat((line - lineNumber).toString().length + 1);
-          const coloredLine = colorize(rawLine);
-          console.log(`${padding}${lineNumber} | ${coloredLine}`);
-        });
-        console.log(" ".repeat(column + 4 + line.toString().length) + "^");
-        const ParsingError = `At [${line},${column}]: Expecting one of ${expecting.join(
-          ", "
-        )} but encountered ${encountered}`;
-        throw ParsingError;
-      }
-    });
+    .then(handleParseResult);
 
-module.exports = { parser, parse };
+module.exports = { parser, parse, handleParseResult };
---
+++ packages/core/tests/language/math.test.js
@@ -0,0 +1,18 @@
+const { compile } = require("../../compiler");
+const fs = require("fs");
+const path = require("path");
+
+test("Compile and run maths", async () => {
+  const file = path.join(__dirname, "./clio/math.clio");
+  const input = fs.readFileSync(file, { encoding: "utf8" });
+  const output = await compile(input, file);
+  const { code } = output.toStringWithSourceMap();
+  const module = { exports: {} };
+  eval(code);
+  const { scope } = module.exports;
+  expect(scope.one.valueOf()).toEqual(1);
+  expect(scope.two.valueOf()).toEqual(2);
+  expect(scope.three.valueOf()).toEqual(3);
+  expect(scope.four.valueOf()).toEqual(4);
+  expect(scope.five.valueOf()).toEqual(5);
+});
