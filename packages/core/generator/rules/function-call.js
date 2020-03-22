b8732cf97050a3fd92e9c2a7327565fea41f4687
Pouya <pouya.eghbali@yandex.com>
2020-03-22 13:03:04 +0100

    Reverted last changes to function call generator rule

--- packages/core/generator/rules/function-call.js
+++ packages/core/generator/rules/function-call.js
@@ -6,12 +6,9 @@ const make = (method, fn, args) => arr`${method}(${fn}, ${args.join(", ")})`;
 class functionCall extends Rule {
   parseCST() {
     const { fn, args, map } = this.cst;
-    const generatedFn = this.generate(fn);
+    const processedFn = this.generate(fn);
     const processedArgs = args.map(arg => this.generate(arg));
     const method = map ? ".map" : ".pipe";
-    const processedFn = generatedFn.startsWith("scope.$")
-      ? `rpc.fn("${generatedFn}")`
-      : generatedFn;
     return make(method, processedFn, processedArgs);
   }
 }
