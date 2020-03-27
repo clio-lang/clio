0e68681b39c400409f06c9b46b6c2e836e9faddc
Pouya <pouya.eghbali@yandex.com>
2020-03-27 17:02:01 +0100

    Improve init script

--- packages/internals/src/rpc.js
+++ packages/internals/src/rpc.js
@@ -1,38 +1,36 @@
+// this should probably be a separate module
+
 const { Dispatcher } = require("clio-rpc/dispatcher");
 const { Worker } = require("clio-rpc/worker");
 const { Executor } = require("clio-rpc/executor");
 const IPC = require("clio-rpc/transports/ipc");
 const os = require("os");
-const cluster = require("cluster");
+const { fork } = require("child_process");
 
 const numCPUs = os.cpus().length;
 
-const fork = () => {
-  console.log("Forking");
-  for (let i = 0; i < numCPUs; i++) cluster.fork();
+const fromKeyValues = (prev, curr) => {
+  prev[curr.key] = curr.value;
+  return prev;
 };
 
-const server = scope => {
-  // imports will be impossible, we need to find a way to recognize the main file
-  if (!scope.$.main) throw new Error("No main function, refusing to run!");
-  module.exports.isServer = true;
-  module.exports.isClient = false;
-  console.log("Starting server");
+const startServer = () => {
+  console.log("Starting server process");
   const dispatcher = new Dispatcher();
   const transport = new IPC.Server();
   dispatcher.addTransport(transport);
-  transport.ipcServer.on("listening", fork);
-  dispatcher.expectWorkers(numCPUs).then(() => {
-    initExecutor();
-    const main = scope.$.main.withContext(true);
-    console.log("Running main");
-    main(process.argv).valueOf();
-  });
+  return dispatcher;
 };
 
-const client = () => {
-  module.exports.isServer = false;
-  module.exports.isClient = true;
+const startWorker = () => {
+  // this is a worker process
+  const { CLIO_WORKER_ID } = process.env;
+  console.log("Starting worker", CLIO_WORKER_ID);
+  const options = Object.keys(process.env)
+    .filter(key => key.startsWith("CLIO_WORKER_"))
+    .map(key => [key.replace(/^CLIO_WORKER_/, ""), process.env[key]])
+    .reduce(fromKeyValues, {});
+  console.log({ options });
   const transport = new IPC.Client();
   const worker = new Worker(transport);
   [...fns.values()].forEach(fn => {
@@ -53,14 +51,51 @@ const client = () => {
   worker.connect();
 };
 
-const init = (scope, config) => {
-  if (cluster.isMaster) server(scope, config);
-  else client(config);
+const forkWorkers = config => {
+  const start = process.argv[1];
+  const { workers } = config;
+  for (const worker of workers) {
+    const { transport, count = numCPUs, ...options } = worker;
+    const OPTIONS = Object.entries(options)
+      .map(([key, value]) => ({
+        key: `CLIO_WORKER_${key.toUpperCase()}`,
+        value
+      }))
+      .reduce(fromKeyValues, {});
+    for (let i = 0; i < count; i++) {
+      fork(start, [], {
+        env: { CLIO_PROCESS_TYPE: "WORKER", CLIO_WORKER_ID: i, ...OPTIONS }
+      });
+    }
+  }
+};
+
+const runMain = scope => {
+  initExecutor();
+  const main = scope.$.main.withContext(true);
+  console.log("Running main");
+  main(process.argv).valueOf();
+};
+
+const startMaster = (scope, config) => {
+  if (!scope.$.main) throw new Error("No main function, refusing to run.");
+  console.log("Starting master process");
+  const dispatcher = startServer();
+  const [transport] = dispatcher.transports;
+  transport.ipcServer.on("listening", () => forkWorkers(config));
+  dispatcher.expectWorkers(numCPUs).then(() => runMain(scope));
 };
 
 const initExecutor = () => {
   const transport = new IPC.Client();
   module.exports.executor = new Executor(transport);
+};
+
+const init = (scope, config) => {
+  const { CLIO_PROCESS_TYPE = "MASTER" } = process.env;
+  if (CLIO_PROCESS_TYPE == "MASTER") startMaster(scope, config);
+  else if (CLIO_PROCESS_TYPE == "SERVER") startServer();
+  else if (CLIO_PROCESS_TYPE == "WORKER") startWorker();
 };
 
 const fns = new Map();
