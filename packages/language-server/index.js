81f264847ce15694197f220bd6d9bc9b2fdbdd53
fwcd <fwcdmail@gmail.com>
2021-03-18 21:59:09 +0100

    Implement skeletal language server

---
+++ packages/language-server/index.js
@@ -0,0 +1,26 @@
+const ls = require("vscode-languageserver/node");
+const doc = require("vscode-languageserver-textdocument");
+
+const connection = ls.createConnection(ls.ProposedFeatures.all);
+const documents = new ls.TextDocuments(doc.TextDocument);
+
+connection.onInitialize(params => {
+  return {
+    capabilities: {
+      textDocumentSync: ls.TextDocumentSyncKind.Incremental,
+      completionProvider: {}
+    }
+  };
+});
+
+connection.onCompletion(params => {
+  return [
+    {
+      label: "Test",
+      kind: ls.CompletionItemKind.Method
+    }
+  ];
+});
+
+documents.listen(connection);
+connection.listen();
