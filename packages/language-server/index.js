f1453367e85f11c0a55c06a055828678d76497e9
fwcd <fwcdmail@gmail.com>
2021-03-18 23:04:08 +0100

    Automatically parse open documents in language server

--- packages/language-server/index.js
+++ packages/language-server/index.js
@@ -2,9 +2,25 @@
 
 const ls = require("vscode-languageserver/node");
 const doc = require("vscode-languageserver-textdocument");
+const url = require("url");
+const { parse } = require("clio-core");
 
 const connection = ls.createConnection(ls.ProposedFeatures.all);
 const documents = new ls.TextDocuments(doc.TextDocument);
+
+const parses = new Map();
+
+documents.onDidOpen(ev => {
+  parses[ev.document.uri] = parse(ev.document.getText(), url.fileURLToPath(ev.document.uri));
+});
+
+documents.onDidChangeContent(ev => {
+  parses[ev.document.uri] = parse(ev.document.getText(), url.fileURLToPath(ev.document.uri));
+});
+
+documents.onDidClose(ev => {
+  parses.delete(ev.document.uri);
+});
 
 connection.onInitialize(params => {
   return {
