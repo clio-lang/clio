e2e6f5734b0a4c2462624d55b6d380b1518ec601
Pouya <pouya.eghbali@yandex.com>
2021-05-01 20:59:00 +0200

    Fix priorities

--- packages/core/syntax/boosters/index.js
+++ packages/core/syntax/boosters/index.js
@@ -2,17 +2,20 @@ const { map } = require("bean-parser");
 const { wrap } = require("../common");
 
 module.exports = {
-  ...map(["symbol", "propertyAccess", "wrapped", "slice", "range"], {
-    ...map(
-      ["groupStart"],
-      wrap(
-        /* istanbul ignore next */
-        () => {
-          /* This never matches, but boosts wrapped, array,
+  ...map(
+    ["symbol", "propertyAccess", "wrapped", "slice", "range", "parallelFn"],
+    {
+      ...map(
+        ["groupStart"],
+        wrap(
+          /* istanbul ignore next */
+          () => {
+            /* This never matches, but boosts wrapped, array,
              hashmap, set vs block rule matching */
-        },
-        0.5
-      )
-    ),
-  }),
+          },
+          0.5
+        )
+      ),
+    }
+  ),
 };
