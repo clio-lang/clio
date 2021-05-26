582c9f0f6021aee9a4b72326b4fb8aad8c8a240c
Pouya <pouya.eghbali@yandex.com>
2021-05-26 20:04:14 +0200

    Fix npm install issues on windows

--- packages/manifest/npm_dependencies.js
+++ packages/manifest/npm_dependencies.js
@@ -13,7 +13,8 @@ function fetchNpmDependencies(destination, silent = false) {
   return new Promise((resolve, reject) => {
     const args = ["install"];
     if (silent) args.push("--silent");
-    const install = spawn("npm", args, { cwd: destination });
+    const npm = process.platform == "win32" ? "npm.cmd" : "npm";
+    const install = spawn(npm, args, { cwd: destination });
     install.on("close", resolve);
     install.on("error", reject);
   });
