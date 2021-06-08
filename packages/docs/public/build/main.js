6c61ff40c905073804330e9a7ee589ef96883e4d
Pouya <pouya.eghbali@yandex.com>
2021-06-08 22:33:40 +0200

    Added dynamic loading + routing / indexing metadata

--- packages/docs/public/build/main.js
+++
@@ -1,4 +0,0 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.getElementsByTagName('head')[0].appendChild(r) })(window.document);
-export { R as default } from './main-2a4365a3.js';
-//# sourceMappingURL=main.js.map
