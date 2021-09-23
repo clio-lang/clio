ab67cb78333ddd5a23865165d76737e758c2a82b
Pouya <pouya.eghbali@yandex.com>
2021-09-23 22:53:30 +0200

    Fix tests

--- packages/core/syntax/boosters/index.js
+++ packages/core/syntax/boosters/index.js
@@ -1,45 +1,58 @@
 const { map, merge } = require("bean-parser");
 const { wrap } = require("../common");
 
-const specials = [
-  "symbol",
-  "propertyAccess",
-  "wrapped",
-  "slice",
-  "parallelFn",
-  "decorator",
-  "decoratorAccess",
-];
-
 module.exports = merge(
   {
-    ...map(specials, {
-      ...map(
-        ["groupStart"],
-        wrap(
-          /* istanbul ignore next */
-          () => {
-            /* This never matches, but boosts wrapped, array,
+    ...map(
+      [
+        "symbol",
+        "propertyAccess",
+        "wrapped",
+        "slice",
+        "range",
+        "parallelFn",
+        "decorator",
+        "decoratorAccess",
+      ],
+      {
+        ...map(
+          ["groupStart"],
+          wrap(
+            /* istanbul ignore next */
+            () => {
+              /* This never matches, but boosts wrapped, array,
              hashmap, set vs block rule matching */
-          },
-          0.5
-        )
-      ),
-    }),
+            },
+            0.5
+          )
+        ),
+      }
+    ),
   },
   {
-    ...map(specials, {
-      ...map(
-        ["hash"],
-        wrap(
-          /* istanbul ignore next */
-          () => {
-            /* This never matches, but boosts wrapped, array,
+    ...map(
+      [
+        "symbol",
+        "propertyAccess",
+        "wrapped",
+        "slice",
+        "parallelFn",
+        "decorator",
+        "decoratorAccess",
+      ],
+      {
+        ...map(
+          ["hash"],
+          wrap(
+            /* istanbul ignore next */
+            () => {
+              /* This never matches, but boosts wrapped, array,
              hashmap, set vs block rule matching */
-          },
-          0.05
-        )
-      ),
-    }),
+            },
+            0.05
+          )
+        ),
+      }
+    ),
   }
 );
