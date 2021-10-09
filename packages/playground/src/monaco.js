db1f9cd39edc66210fe123e0bba06671f460066d
Pouya <pouya.eghbali@yandex.com>
2021-10-09 16:44:44 +0200

    Update docs

--- packages/playground/src/monaco.js
+++ packages/playground/src/monaco.js
@@ -120,13 +120,6 @@ export const clio = {
       [/'/, { token: "string.quote", bracket: "@open", next: "@stringSingle" }],
     ],
 
-    comment: [
-      [/[^-+]+/, "comment"],
-      [/\+-/, "comment", "@push"], // nested comment
-      [/-\+/, "comment", "@pop"],
-      [/[-+]/, "comment"],
-    ],
-
     string: [
       [/[^\\"]+/, "string"],
       [/@escapes/, "string.escape"],
@@ -143,7 +136,6 @@ export const clio = {
 
     whitespace: [
       [/[ \t\r\n]+/, "white"],
-      [/\+-/, "comment", "@comment"],
       [/--.*$/, "comment"],
     ],
 
